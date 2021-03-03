5e9f8175a4f126a54580f6fdab8537be48936bb5
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2021-03-02 20:59:30 -0600

    playing with the forLoop

--- ForLoops/forL.js
+++ ForLoops/forL.js
@@ -4,6 +4,14 @@ var text = "";
 var i;
 for (i = 0; i < cars.length; i++) {
   text += cars[i] + "<br>";
+  console.log(cars[i])
 }
-console.log(i)
+
+}
+
+function ForLoop2() {
+for (let step = 0; step < 5; step++) {
+  // Runs 5 times, with values of step 0 through 4.
+  console.log('Walking east one step');
+  }
 }
