ce648b29f2c156e7ea630d877411c1edf7fcb422
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2021-03-02 21:19:11 -0600

    bubbleSort function

--- ForLoops/forL.js
+++ ForLoops/forL.js
@@ -15,3 +15,26 @@ for (let step = 0; step < 5; step++) {
   console.log('Walking east one step');
   }
 }
+
+let unsortedArr = [10, 5, 12, 7, 3, 21, 14, 16, 1];
+let swapped;
+
+function bubbleSort(arr) {
+  swapped = false;
+  let end = arr.length - 1;
+  for (let i = 0; i < end; i++) {
+    if (arr[i] > arr[i + 1]){
+      swapped = true;
+      let temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+    }
+  }
+  end--;
+}
+
+do {
+  bubbleSort(unsortedArr);
+} while (swapped);
+
+console.log(unsortedArr);
